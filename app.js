cca60c5653fc654e536b35f6a529399a0c639c56
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2021-09-03 22:39:02 +0530

    style: style.css added succesfully

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-btn");
 const outputBox = document.querySelector("#output-box");
 
+outputBox.style.display = "none";
+
 function isYourBirthdateLucky() {
     var dob = dateOfBirth.value;
     var result = calculateSum(dob);
+    
 
+    if (dob&&result){
+    outputBox.style.display = "block";
     if (Number(result) % Number(luckyNumber.value) === 0) {
-        outputBox.innerText = "yess";
+        outputBox.innerText = "Woah!!, your Birthday is Luckiest.";
 
-    } else { outputBox.innerText = "noooo"; }
+    } else { outputBox.innerText = "Oops! Your birthday is not that lucky!"; }
+    
+} else {outputBox.style.display = "block"; 
+    outputBox.innerText = "please enter both values"; }
+
 }
 
 
